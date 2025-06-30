2115ad8033ef70ae6d55d9058de01279c815d9fe
Rieversed <LunarScytheYT@gmail.com>
2025-06-30 14:45:04 +0200

    Create EzGit.bat

---
+++ EzGit.bat
@@ -0,0 +1,111 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:menu
+cls
+echo ========================================
+echo        GIT HELPER SCRIPT
+echo ========================================
+echo.
+echo Current branch:
+git branch --show-current
+echo.
+echo What do you want to do?
+echo.
+echo 1. List branches
+echo 2. Switch branch
+echo 3. Pull latest changes
+echo 4. Add, commit, and push
+echo 5. Create new branch
+echo 6. Exit
+echo 7. Create tag (release)
+echo 8. Delete tag (release)
+echo.
+set /p choice=Enter your choice (1-8): 
+
+if "%choice%"=="1" goto list
+if "%choice%"=="2" goto switch
+if "%choice%"=="3" goto pull
+if "%choice%"=="4" goto commitpush
+if "%choice%"=="5" goto create
+if "%choice%"=="6" exit
+if "%choice%"=="7" goto createtag
+if "%choice%"=="8" goto deletetag
+goto menu
+
+:list
+cls
+echo ========================================
+echo           All Branches
+echo ========================================
+git branch -a
+pause
+goto menu
+
+:switch
+cls
+echo ========================================
+echo           Switch Branch
+echo ========================================
+git branch
+echo.
+set /p branch=Enter branch name to switch to: 
+git checkout %branch%
+pause
+goto menu
+
+:pull
+cls
+echo ========================================
+echo           Pull Latest Changes
+echo ========================================
+git pull
+pause
+goto menu
+
+:commitpush
+cls
+echo ========================================
+echo       Add, Commit, and Push
+echo ========================================
+set /p msg=Enter commit message: 
+git add .
+git commit -m "%msg%"
+git push
+pause
+goto menu
+
+:create
+cls
+echo ========================================
+echo         Create New Branch
+echo ========================================
+set /p newbranch=Enter new branch name: 
+git checkout -b %newbranch%
+git push -u origin %newbranch%
+pause
+goto menu
+
+:createtag
+cls
+echo ========================================
+echo          Create Tag (Release)
+echo ========================================
+set /p tagname=Enter tag name: 
+git tag %tagname%
+git push origin %tagname%
+pause
+goto menu
+
+:deletetag
+cls
+echo ========================================
+echo          Delete Tag (Release)
+echo ========================================
+git tag
+echo.
+set /p deltag=Enter tag name to delete: 
+git tag -d %deltag%
+git push origin :refs/tags/%deltag%
+pause
+goto menu
